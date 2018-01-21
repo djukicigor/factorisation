d8b9c5c15e5c282520bdb8a5d9f17ef70f13e095
web-man <igordjukic0@gmail.com>
2018-01-21 02:16:28 +0100

    First commit

---
+++ src/main/java/com/example/factorisation/FactorisationApplication.java
@@ -0,0 +1,12 @@
+package com.example.factorisation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FactorisationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FactorisationApplication.class, args);
+	}
+}
