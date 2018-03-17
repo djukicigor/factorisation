dff5107fba60393ffa9159de0e679ba5106e35fc
Aleksandar Miladinovic <acamiladinovic95@hotmail.com>
2018-03-17 18:11:59 +0100

    Add model Company

--- src/main/java/com/example/factorisation/model/City.java
+++ src/main/java/com/example/factorisation/model/City.java
@@ -1,10 +1,17 @@
 package com.example.factorisation.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
 
 @Entity
 public class City {
@@ -18,6 +25,9 @@ public class City {
 	
 	@Column(name="D_CODE", columnDefinition="CHAR(5)")
 	private String postalCode;
+	
+	@OneToMany(mappedBy="city", cascade=CascadeType.ALL, fetch=FetchType.LAZY)
+	private List<Company> companies = new ArrayList<Company>();
 	
 	public City() {
 	}
@@ -49,6 +59,27 @@ public class City {
 
 	public void setPostalCode(String postalCode) {
 		this.postalCode = postalCode;
+	}
+	
+	public List<Company> getCompanies() {
+		return companies;
+	}
+
+	public void setCompanies(List<Company> companies) {
+		this.companies = companies;
+	}
+	
+	public void addCompany(Company company){
+		this.companies.add(company);
+		
+		if(company.getCity()!=this){
+			company.setCity(this);
+		}
+	}
+	
+	public void removeCompany(Company company){
+		company.setCity(null);
+		companies.remove(company);
 	}
 
 }
---
+++ src/main/java/com/example/factorisation/model/Company.java
@@ -0,0 +1,118 @@
+package com.example.factorisation.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+
+@Entity
+public class Company {
+	
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+	
+	@Column(name="Name", columnDefinition="VARCHAR(40)")
+	private String name;
+	
+	@Column(name="Address", columnDefinition="VARCHAR(40)")
+	private String address;
+	
+	@Column(name="PIB", columnDefinition="CHAR(9)")
+	private String pib;
+	
+	@Column(name="Number", columnDefinition="VARCHAR(15)")
+	private String number;
+	
+	@Column(name="Email", columnDefinition="VARCHAR(40)")
+	private String email;
+	
+	@Column(name="Logo", columnDefinition="VARCHAR(40)")
+	private String logo;
+	
+	@ManyToOne(fetch=FetchType.EAGER)
+	private City city;
+	
+	public Company() {
+	}
+
+	public Company(String name, String address, String pib, String number, String email, String logo) {
+		this.name = name;
+		this.address = address;
+		this.pib = pib;
+		this.number = number;
+		this.email = email;
+		this.logo = logo;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getPib() {
+		return pib;
+	}
+
+	public void setPib(String pib) {
+		this.pib = pib;
+	}
+
+	public String getNumber() {
+		return number;
+	}
+
+	public void setNumber(String number) {
+		this.number = number;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getLogo() {
+		return logo;
+	}
+
+	public void setLogo(String logo) {
+		this.logo = logo;
+	}
+
+	public City getCity() {
+		return city;
+	}
+
+	public void setCity(City city) {
+		this.city = city;
+		if(city!=null && !city.getCompanies().contains(this)){
+			city.getCompanies().add(this);
+		}
+	}
+}
