5c5c595b803993eff883b0022b7b85adcb729e47
Igor Djukic <igordjukic0@gmail.com>
2018-04-16 05:08:40 +0200

    Fixed post for adding goods into price list

--- src/main/java/com/example/factorisation/converter/PricelistItemsDTOtoPricelistItems.java
+++ src/main/java/com/example/factorisation/converter/PricelistItemsDTOtoPricelistItems.java
@@ -2,17 +2,24 @@ package com.example.factorisation.converter;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
 
+import com.example.factorisation.model.GoodsOrServices;
 import com.example.factorisation.model.Pricelist;
 import com.example.factorisation.model.PricelistItems;
+import com.example.factorisation.service.GoodsOrServicesService;
 import com.example.factorisation.service.PricelistService;
 
 import dto.PricelistItemsDTO;
 
+@Component
 public class PricelistItemsDTOtoPricelistItems implements Converter<PricelistItemsDTO, PricelistItems> {
 
 	@Autowired
 	private PricelistService pricelistService;
+	
+	@Autowired
+	private GoodsOrServicesService goodsOrServicesService;
 	
 	@Override
 	public PricelistItems convert(PricelistItemsDTO arg0) {
@@ -25,6 +32,11 @@ public class PricelistItemsDTOtoPricelistItems implements Converter<PricelistIte
 		Pricelist pricelist = pricelistService.findOne(arg0.getPricelistId());
 		if(pricelist!=null) {
 			p.setPricelist(pricelist);
+		}
+		
+		GoodsOrServices goodsOrService = goodsOrServicesService.findOne(arg0.getPricelistId());
+		if(goodsOrService!=null) {
+			p.setGoodsOrServices(goodsOrService);
 		}
 		
 		return p;
